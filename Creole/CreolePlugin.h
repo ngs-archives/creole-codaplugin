3cee36c08fda9f2d96b030cb457663f2e41f965d
Atsushi NAGASE <atsn.ngs@gmail.com>
2012-05-28 04:17:54 +0900

    externalize shaded classes/resources

--- Creole/CreolePlugin.h
+++ Creole/CreolePlugin.h
@@ -6,13 +6,8 @@
 //  Copyright (c) 2012 LittleApps Inc. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
-#import "CodaPlugInsController.h"
+#import "WikiPlugin.h"
 
-@interface CreolePlugin : NSObject<CodaPlugIn>
-
-@property (strong) NSURL *bundleURL;
-
-- (void)didPreviewClose;
+@interface CreolePlugin : WikiPlugin
 
 @end
